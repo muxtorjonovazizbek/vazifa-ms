3bb67051e7c2133f2b7de3710b5b3566fa75f722
muxtorjonovazizbek <azizbekmukhtorjonov581@gmail.com>
2024-07-27 18:35:25 +0500

    done

--- app.js
+++ app.js
@@ -126,5 +126,64 @@
 //  console.log(separateEvenOdd([1, 2, 3, 4, 5, 6, 7, 8, 9])); // Output: { even: [2, 4, 6, 8], odd: [1, 3, 5, 7, 9]
 
 
+//  87. Berilgan raqamlar ro'yxatidagi tub sonlarni toping.
 
+// function findPrime(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         // console.log(arr[i]);
+//         if (arr[i] > 1 && arr[i] % arr[i] == 0) {
+//             console.log(arr[i]);
+//         }
+        
+//     }
+//  }
+ 
+//  // Test case
+//  console.log(findPrime([2, 3, 4, 5, 6, 7])); // Output: [2, 3, 5, 7]
+
+
+
+// 88. Berilgan raqamlar ro'yxatidagi barcha elementlarni k qadam bosib aylanma tartibda o'ngga suring.
+
+// function rotateArray(arr, k) {
+//     for (let i = 0; i < k; i++) {
+//         arr.unshift(arr.pop())
+//     }
+//     return arr
+// }
+
+// // Test case
+// console.log(rotateArray([1, 2, 3, 4, 5], 2)); // Output: [4, 5, 1, 2, 3]
+
+
+// 89. Berilgan raqamlar ro'yxatidagi o'rtacha qiymatni toping, faqat ikkita eng kichik va ikkita eng katta qiymatni hisobga olmasdan.
+
+// function trimmedMean(arr) {
+//     let total = 0
+//     arr.forEach(element => {
+//         total += element
+//     });
+//     console.log(total / arr.length);
+// }
+
+// // Test case
+// console.log(trimmedMean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // Output: 5.5
+
+
+
+// 90. Berilgan raqamlar ro'yxatidagi barcha polindrom sonlarni toping.
+
+
+function isPalindrome(num) {
+    let str = num.toString();
+    return str === str.split('').reverse().join('');
+}
+
+function findPalindromes(arr) {
+    let result = arr.filter(isPalindrome);
+    return result;
+}
+
+// Test case
+console.log(findPalindromes([121, 123, 202, 345, 454])); // Output: [121, 202, 454]
 
